dffba87465b6d204577d6366c06a50959820b410
David McGregor <davidjohnmcgregor@gmail.com>
2024-01-30 10:50:01 -0500

    Get visibility on palette change

--- components/welcome/index.js
+++ components/welcome/index.js
@@ -7,12 +7,24 @@ const defaultPalette = { 'R': '255', 'G': '0', 'B': '0'}
 const Welcome = () => {
   const [palette, setPalette] = useState(defaultPalette);
 
+  const handleColorChange = (e) => {
+    const { name, value } = e.target;
+    setPalette(prevPalette => ({
+      ...prevPalette,
+      [name]: value
+    }));
+  };
+
   const paletteColor = `rgb(${palette.R}, ${palette.G},${palette.B})`
   
   return (
     <div className={s.welcomeContainer}>
       <h1 className={s.welcome}>Create Palette</h1>
       <div className={s.colorSquare} style={{ backgroundColor: paletteColor }} />
+      {paletteColor}
+      <input type="range" min="0" max="255" value={palette.R} name="R" id="R" onChange={handleColorChange} />
+      <input type="range" min="0" max="255" value={palette.G} name="G" id="G" onChange={handleColorChange} />
+      <input type="range" min="0" max="255" value={palette.B} name="B" id="B" onChange={handleColorChange} />
     </div>
   );
 };
